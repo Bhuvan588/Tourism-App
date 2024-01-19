d6d6890f1184eae0e3772266f78aa58562e49bd0
Bhuvan588 <bhuvan27112003@gmail.com>
2024-01-19 16:25:25 +0530

    No changes in code.

--- app/src/main/java/com/example/mad_final/PlaceAdapter.java
+++ app/src/main/java/com/example/mad_final/PlaceAdapter.java
@@ -12,7 +12,6 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-import com.example.mad_final.R;
 
 import java.util.List;
 
